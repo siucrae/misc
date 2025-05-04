ca0822b5d645fbf77908cf2d0dda9f7ee983e6d0
kemper <kemper.t.mullaney@gmail.com>
2025-05-03 22:15:33 -0600

    Update my_combined_sink.sh
    made a delayed sink so i can use my dual pc OBS setup properly over srt

--- bash/my_combined_sink.sh
+++ bash/my_combined_sink.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
 
 sleep 8
-# wait 8 seconds so that it doesnt get consumed by my audio restart & then make the sink
 
+# create main virtual sinks
 pactl load-module module-null-sink media.class=Audio/Sink sink_name=discord-sink channel_map=stereo
 pactl load-module module-null-sink media.class=Audio/Sink sink_name=game-sink channel_map=stereo
 pactl load-module module-null-sink media.class=Audio/Sink sink_name=browser-sink channel_map=stereo
+
+# create delayed sinks with descriptions for qtwgraph
+pactl load-module module-null-sink \
+  sink_name=delayed_sink \
+  sink_properties=device.description="delayed_input"
+
+pactl load-module module-null-sink \
+  sink_name=delayed-sink-output \
+  sink_properties=device.description="delayed_output"
+
+# create loopback with 4.5s delay
+pactl load-module module-loopback \
+  source=delayed_sink.monitor \
+  sink=delayed-sink-output \
+  latency_msec=4500 \
+  sink_dont_move=true \
+  source_dont_move=true \
+  source_output_properties='node.description=DelayedLoopback media.name=DelayedLoopback'
